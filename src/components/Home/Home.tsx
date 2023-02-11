dccdba2db831873c6b0e1f61c035d948dccfc3aa
FalkKi <nochkorzi@gmail.com>
2023-02-11 13:12:29 +0300

    This is test message

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from './../../redux/store';
 import { fetchCVs, postCV } from './../../requests/cvRequests';
 import styles from '../../app.module.css';
-import PersonalData from "../PersonalData/PersonalData";
-import axios from "axios";
 import PersonalDataContainer from './../PersonalData/PersonalDataContainer';
 
 
@@ -16,8 +14,9 @@ const Home: React.FC = () => {
    return (
       <div className={styles.container}>
          <div className={styles.personInfo}>
-            <PersonalDataContainer/>
+            <PersonalDataContainer />
          </div>
+         <div>Test</div>
       </div>
    );
 };
