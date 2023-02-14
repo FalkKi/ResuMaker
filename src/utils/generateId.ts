274ba0f9626e277535ef27ae37f0e08c2da79b77
FalkKi <nochkorzi@gmail.com>
2023-02-14 19:12:00 +0300

    new

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -4,16 +4,3 @@ export const generateId = () => {
    return id;
 };
 
-// export const getUserInfoData = (data: UserWorkHistory | LanguageInfoType | UserEducationHistory,
-//    param: "educationHistory" | "workHistory" | "languages") => {
-//    const index = userInfo[param].findIndex(el => el.id === data.id);
-
-//    if (index !== -1) {
-//       userInfo[param][index] = data;
-//    } else{
-//       setUserInfo(prevState => ({
-//          ...prevState,
-//          param: prevState[param].concat(data)
-//       }));
-//    };
-// };
